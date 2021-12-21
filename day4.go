970f3c86d5e9ed52063da7ac681e3ababb7330ed
Attila Tajti <attila.tajti@gmail.com>
2021-12-21 14:25:16 +0100

    day 4/1

---
+++ day4.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/tajtiattila/aoc"
+	"github.com/tajtiattila/aoc/input"
+)
+
+func init() {
+	aoc.Register(4, day4)
+}
+
+func day4() {
+	puzzle, err := parsebingo(aoc.Reader(4))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	score := 0
+Outer:
+	for _, d := range puzzle.draw {
+		for _, c := range puzzle.card {
+			c.mark(d)
+			if c.win() {
+				score = c.score(d)
+				break Outer
+			}
+		}
+	}
+
+	fmt.Println("Day 4/1:", score)
+
+}
+
+type bingocell struct {
+	value int
+	mark  bool
+}
+
+type bingocard []bingocell
+
+func (c bingocard) resetmarks() {
+	for i := range c {
+		c[i].mark = false
+	}
+}
+
+func (c bingocard) mark(n int) {
+	for i := range c {
+		if c[i].value == n {
+			c[i].mark = true
+		}
+	}
+}
+
+func (c bingocard) win() bool {
+	for i := 0; i < 5; i++ {
+		if c.winrow(i) || c.wincol(i) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c bingocard) winrow(row int) bool {
+	ofs := 5 * row
+	for _, x := range c[ofs : ofs+5] {
+		if !x.mark {
+			return false
+		}
+	}
+	return true
+}
+
+func (c bingocard) wincol(col int) bool {
+	for i := col; i < 25; i += 5 {
+		if !c[i].mark {
+			return false
+		}
+	}
+	return true
+}
+
+func (c bingocard) score(lastdraw int) int {
+	var u int
+	for _, x := range c {
+		if !x.mark {
+			u += x.value
+		}
+	}
+	return u * lastdraw
+}
+
+type bingopuzzle struct {
+	draw []int
+	card []bingocard
+}
+
+func parsebingo(r io.Reader) (*bingopuzzle, error) {
+	var draw []int
+	var card []bingocard
+	scanner := input.NewBlockScanner(r)
+	for scanner.Scan() {
+		var err error
+		if draw == nil {
+			draw, err = parsedraw(scanner.Text())
+		} else {
+			var c bingocard
+			c, err = parsebingocard(scanner.Text())
+			card = append(card, c)
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &bingopuzzle{
+		draw: draw,
+		card: card,
+	}, scanner.Err()
+}
+
+func parsedraw(s string) ([]int, error) {
+	var v []int
+	for _, x := range strings.Split(s, ",") {
+		n, err := strconv.Atoi(x)
+		if err != nil {
+			return nil, err
+		}
+		v = append(v, n)
+	}
+	return v, nil
+}
+
+func parsebingocard(s string) (bingocard, error) {
+	var v []bingocell
+	for _, x := range strings.Fields(s) {
+		n, err := strconv.Atoi(x)
+		if err != nil {
+			return nil, err
+		}
+		v = append(v, bingocell{value: n})
+	}
+
+	if len(v) != 25 {
+		return nil, fmt.Errorf("Invalid bingo card: %s", s)
+	}
+
+	return bingocard(v), nil
+}
